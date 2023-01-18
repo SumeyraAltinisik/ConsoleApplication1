252824b8243146b78470ca5034dc69a34f2cac75
SumeyraAltinisik <sfa2023@gmail.com>
2023-01-18 22:29:24 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+// deðiþkenlerin kullanýmý
+//int sayi1=90, x=25, sonuc;
+//double pi = 3.14;
+//char y = 'a';
+//string metin = "lkfvjlkv";
+//float sayi1 = 25, sayi2 = 10, toplam, fark, carpim, bolum, mod;
+//int x = 5, y = 2;
+//int x = 5, sonuc = 0;
+//int x = 286, y = 264;
+//string metin = "Enstitu istanbul ismek - ibb";
+//string egitim = "C# programlama egitimi";
+//string x;
+//float x;
+int x = 5;
+int main()
+{
+    /*sonuc = sayi1 + x;
+    cout << sonuc<<endl;
+    cout << metin;*/
+    /* toplam= sayi1 + sayi2;
+     fark = sayi1 - sayi2;
+     carpim = sayi1 * sayi2;
+     bolum = sayi1 / sayi2;
+     mod = x % y;
+    
+     cout << toplam << endl;
+     cout << fark << endl;
+     cout << carpim << endl;
+     cout << bolum << endl;
+     cout << mod << endl;*/
+
+    /*sonuc = x + 5;
+    cout << sonuc << endl;
+    sonuc = x - 5;
+    cout << sonuc << endl;
+    sonuc = x * 5;
+    cout << sonuc << endl;
+    sonuc = x / 5;
+    cout << sonuc << endl;*/
+
+    /*cout << (x < y) << endl;
+    cout << (x > y) << endl;
+    cout << (x <= y) << endl;
+    cout << (x >= y) << endl;
+    cout << (x == y) << endl;*/
+
+    /*cout << metin.length()<<endl;
+    cout << metin.size()<< endl;
+    cout << metin[0] << metin[1]<<"..."<<metin[8]<<endl;
+    
+    egitim[1] = '+';
+    egitim[2] = '+';
+    cout << egitim;*/
+
+    //getline(cin, x);
+    //cout <<"Klavyeden girilen deger= "<< x;
+    /*cout << "Lutfen bir sayi giriniz: ";
+    cin >> x;
+    cout <<"girilen sayinin sinus("<<x<<")="<< sin(x) <<endl;
+    cout <<"girilen sayinin cosinus("<<x<<")="<< cos(x)<<endl;
+    cout <<"girilen sayinin tanjant("<<x<<")="<< tan(x)<<endl;
+    cout <<"girilen sayinin karesi("<<x<<")="<< sqrt(x);*/
+
+    if (x >= 5)
+    {
+        cout << "Merhaba";
+    }
+    else
+    {
+        cout << "ismek";
+    }
+}
+
+
